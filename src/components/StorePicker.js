ceb1743a17fee815341d1cb54f4f36a345455856
Vatsal <ramivatsal07@gmail.com>
2020-04-03 15:21:44 +0530

    imported css

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,11 +3,11 @@ import React, {Fragment} from 'react';
 class StorePicker extends React.Component {
     render() {
         return (
-            <Fragment>
+            <form className="store-selector">
                 <h2>Please Enter Store</h2>
-                <input type="text" placeholder="Store Name"  required/>
+                <input type="text" placeholder="Store Name" required />
                 <button type="submit">Visit Store -></button>
-            </Fragment>
+            </form>
         )
     }
 }
