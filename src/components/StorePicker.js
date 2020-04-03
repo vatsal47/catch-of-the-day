eff162d148ced91e25c5062d50c0a5410fb248ab
Vatsal <ramivatsal07@gmail.com>
2020-04-03 20:15:01 +0530

    made app component with header,order and inventory component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,10 @@
-import React, {Fragment} from 'react'; 
+import React from 'react'; 
 
 class StorePicker extends React.Component {
     render() {
         return (
             <form className="store-selector">
-                <h2>Please Enter Store</h2>
+                    <h2>Please Enter Store</h2>
                 <input type="text" placeholder="Store Name" required />
                 <button type="submit">Visit Store -></button>
             </form>
